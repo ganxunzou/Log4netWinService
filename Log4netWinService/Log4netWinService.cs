936e7ef392641f4ecf6b9e30dd9a81db83ce163f
ganxunzou <ganxunzou@163.com>
2018-07-13 13:28:37 +0800

    init

---
+++ Log4netWinService/Log4netWinService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using log4net;
+
+namespace Log4netWinService
+{
+    public partial class Log4netWinService : ServiceBase
+    {
+        private Timer timerTenSecond = new Timer(10000);
+        ILog log = LogManager.GetLogger(typeof(Log4netWinService));
+
+        public Log4netWinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("Started - Log4netWinService");
+            timerTenSecond.Elapsed += new ElapsedEventHandler(TimerTenSecond_Elapsed);
+            timerTenSecond.Enabled = true;
+        }
+        private void TimerTenSecond_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            log.Error("Triggered - Log4netWinService - Error");
+        }
+
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ Log4netWinService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Log4netWinService
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Log4netWinService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
